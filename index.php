e0171c81a68b7ea68a3c5c948d9f02319730989c
raissaasantos <raissasantos55@yahoo.com>
2024-05-22 01:08:11 -0300

    Inicial steps

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css"
+    rel="stylesheet"
+/>
+  <link rel="stylesheet" href="css/style.css">  
+    <title>Assembléia de Deus Ministério O Atalaia</title>
+</head>
+<body>
+    <nav class="nav_logo">
+        <img src="img/logo.png" alt="Assembléia de Deus Ministério O Atalaia" width="60">
+        Assembléia de Deus Ministério O Atalaia
+        <a href="pages/login.php">Login</a>
+        
+    </nav>
+    <hr>
+    <main>
+        <h1>Sobre nós</h1>
+        <p>
+            A nossa igreja foi iniciada com o intuito de ajudar dependentes químicos, nasceu de um projeto social de recuperação e recolocação desta pessoas em ambientes de trabalho. Além deste trabalhos social ajudamos pessoas em situação de rua ou extrema pobreza. Esses serviços incluem entrega de marmitas para pessoas em situação de rua ou montagem de cestas básicas para famílias que não tem condições financeiras de se manter sozinhas. Através destas trabalhos famílias tem se rendido aos pés da cruz e aceitado Jesus com único e suficiente salvador. Este tem sido o nosso propósito a 11 anos!
+        </p>
+
+        <h1>Ação Social</h1>
+        <p>
+            Foi iniciada em 2013 junto a fundação da nossa igreja. Atualmente, é feita pelo menos duas vezes ao mês, isso inclui entrega de marmitas para pessoas em situação de rua e cestas básicas para pessoas necessitadas.
+        </p>
+    </main>
+    <hr>
+    <footer>
+        <h1>Onde nos encontrar</h1>
+        <address>
+            Estrada do Morro Grande, 1270 - Jardim Ísis (sede) <br>
+            Rua da Paz, 76 - Jardim Cotia <br>
+            Rua Urca, 537 - Parque do Agreste
+        </address>
+
+        <div class="nav_contact">
+            <div class="nav_contact_num">
+                <span><i class="ri-phone-line"></i></span>
+                <div>
+                    <p>Entre em contato</p>
+                    <h4>(11) 995357506</h4>
+                    <h4>(11) 997361270</h4>
+                    <a href="https://www.instagram.com/m_oatalaia" target="_blank"><i class="ri-instagram-line"></i> @m_oatalaia</a>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
