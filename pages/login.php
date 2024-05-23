2cfb75535d3e04109def5387ddacf140626e2b7e
Matthla <matheusguimaraesjacobs@gmail.com>
2024-05-23 19:00:02 -0300

    Update login.php

---
+++ pages/login.php
@@ -0,0 +1,27 @@
+<h1>CADASTRA-SE AQUI</h1>
+ <form action="">
+    <label for="name">Nome</label>
+    
+    <input 
+    type="text"
+    id="Name"
+    name="name"
+    placeholder="Seu nome"
+    required
+    />
+      </form>
+
+  <label for="name">Email</label>
+  
+  <input 
+  type="email"
+  id="Email"
+  name="email"
+  placeholder="seu email"
+  required
+  />
+    </form>
+
+    <button type="submit">Enviar!</button>
+
+</footer>
